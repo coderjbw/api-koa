941e8974cb1c3c5e8eb0139f84fbde5a9dd8b26e
jbwmowen <771056781@qq.com>
2025-04-10 00:40:13 +0800

    增加返回内容

--- controller/chat.js
+++ controller/chat.js
@@ -26,11 +26,52 @@ class ChatController {
             stream: true,
             tools
         });
+        ctx.status = 200;
+        let functionName = '';
+        let requireParameters = '';
+        let lastMessage = [];
         for await (const chunk of completion) {
             const str = JSON.stringify(chunk);
             const obj = JSON.parse(str);
             console.log(str);
             console.log(obj);
+            // 判断工具调用
+            const choices = obj.choices[0].delta;
+            if (choices.content === null && choices.tool_calls) {
+                // console.log("用工具调用-----------");
+                if (messages[messages.length - 1].role !== "assistant") {
+                    messages.push({ role: "assistant", content: "", tool_calls: [] });
+                    lastMessage = messages[messages.length - 1];
+                }
+                // 模型回复的要调用的工具和调用工具时需要的参数取出来
+                const toolCalls = choices.tool_calls;
+                if (toolCalls.length > 0) {
+                    if (lastMessage.tool_calls.length <= 0) {
+                        functionName = toolCalls[0].function.name;
+                        lastMessage.tool_calls.push(toolCalls[0]);
+                    }
+                }
+                // 遍历取出函数参数
+                toolCalls.forEach((item) => {
+                    if (item.function.arguments) {
+                        requireParameters += item.function.arguments;
+                    }
+                    lastMessage.tool_calls[0].function.arguments = requireParameters;
+                });
+            }
+            // 等遍历完毕把工具调用的结果返给前端
+            if (obj.choices[0].finish_reason == "tool_calls") {
+                const resObj = JSON.stringify({ type: "function", functionName, data: JSON.parse(requireParameters) });
+                const buffer = Buffer.from(resObj);
+                ctx.res.write(buffer);
+            }
+            // 没有工具调用
+            if (choices.content) {
+                // console.log("没有工具调用-------");
+                const resObj = JSON.stringify({ type: "content", functionName: "", data: choices.content });
+                const buffer = Buffer.from(resObj);
+                ctx.res.write(buffer);
+            }
         }
     }
 }
